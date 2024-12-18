c878bf2153d36ed604c5f79ed86dc0a8175ab7ad
M.Fawareh <M.Fawareh@MOH-FAWAREH>
2024-12-18 16:59:56 +0300

    updated

---
+++ MyWindowsService/ProjectInstaller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace MyWindowsService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+
+            // ServiceProcessInstaller
+            ServiceProcessInstaller serviceProcessInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem // You can change this to other accounts as needed
+            };
+
+            // ServiceInstaller
+            ServiceInstaller serviceInstaller = new ServiceInstaller
+            {
+                StartType = ServiceStartMode.Automatic, // The service will start automatically
+                ServiceName = "MyWindowsService", // Must match the service name in your code
+                DisplayName = "My Windows Service", // This is what shows up in Services.msc
+                Description = "This is a sample Windows Service for demonstration purposes." // Service description
+            };
+
+            // Add installers to the Installers collection
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
---
+++ MyWindowsService/Service1.cs
@@ -0,0 +1,93 @@
+﻿//using MedicalWCF.ServiceMethod;
+using System;
+using System.Configuration;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace MedicalWindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timerCorporate = new Timer();
+        Timer timerRetail = new Timer();
+        //Financial financialService = new Financial();
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("General", "------------------------------------------Service is started at " + DateTime.Now);
+
+            // Timer for Corporate task
+            timerCorporate.Elapsed += new ElapsedEventHandler(OnCorporateTask);
+            timerCorporate.Interval = Convert.ToDouble(ConfigurationManager.AppSettings["CorporateTimerInterval"]);
+            timerCorporate.Enabled = true;
+
+            // Timer for Retail task
+            timerRetail.Elapsed += new ElapsedEventHandler(OnRetailTask);
+            timerRetail.Interval = Convert.ToDouble(ConfigurationManager.AppSettings["RetailTimerInterval"]);
+            timerRetail.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("General", "------------------------------------------Service is stopped at " + DateTime.Now);
+            timerCorporate.Enabled = false;
+            timerRetail.Enabled = false;
+        }
+
+
+        private void OnCorporateTask(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Corporate", "-------Start: LoadCustomerCorporateToKafka execution at " + DateTime.Now);
+            try
+            {
+                //financialService.LoadCustomerCorporateToKafka();
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Corporate", "Error in LoadCustomerCorporateToKafka: " + ex.Message);
+            }
+            WriteToFile("Corporate", "-------End: LoadCustomerCorporateToKafka execution at " + DateTime.Now);
+        }
+
+
+        private void OnRetailTask(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Retail", "-------Start: LoadCustomerRetailToKafka execution at " + DateTime.Now);
+            try
+            {
+                //financialService.LoadCustomerRetailToKafka();
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Retail", "Error in LoadCustomerRetailToKafka: " + ex.Message);
+            }
+            WriteToFile("Retail", "-------End: LoadCustomerRetailToKafka execution at " + DateTime.Now);
+        }
+
+
+        public void WriteToFile(string fileIdentifier, string message)
+        {
+            string path = ConfigurationManager.AppSettings["WSLogs"];
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            // File name based on the identifier (Corporate, Retail, General)
+            string filepath = path + $"\\{fileIdentifier}_Log_{DateTime.Now.ToShortDateString().Replace('/', '_')}.txt";
+
+            // Append to the file
+            using (StreamWriter sw = File.AppendText(filepath))
+            {
+                sw.WriteLine(message);
+            }
+        }
+    }
+
+}
